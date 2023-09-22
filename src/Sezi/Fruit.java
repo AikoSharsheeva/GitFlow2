6c9cc48c799b6ebcabfa1c41a1e2524eaf9dd798
sezim <osmsezimka@gmail.com>
2023-09-22 08:27:47 +0600

    use this keyWord2 fix class name and constructor name

--- src/Sezi/Fruit.java
+++ src/Sezi/Fruit.java
@@ -2,23 +2,23 @@ package Sezi;
 
 public class Fruit {
     // create your class here
-    class Fruit{
+    class Fruit2{
         public String name;
         public int quantity;
         public double pricePerFruit;
         public boolean isOrganic;
 
-        public Fruit(String name){
+        public Fruit2(String name){
             this.name = name;
         }
 
-        public Fruit(String name, int quantity){
+        public Fruit2(String name, int quantity){
             this.name = name;
             this.quantity = quantity;
 
         }
 
-        public Fruit(String name, int quantity, double pricePerFruit){
+        public Fruit2(String name, int quantity, double pricePerFruit){
             this.name = name;
             this.quantity = quantity;
             this.pricePerFruit = pricePerFruit;
@@ -28,7 +28,7 @@ public class Fruit {
 
 
 
-        public Fruit(String name, int quantity, double pricePerFruit, boolean isOrganic){
+        public Fruit2(String name, int quantity, double pricePerFruit, boolean isOrganic){
             this.name = name;
             this.quantity = quantity;
             this.pricePerFruit = pricePerFruit;
